fb44b039e05abca44ea67377ca70c2430aeee2fe
William Dens <denswilliam@hotmail.com>
2017-12-20 11:35:09 +0100

    Contract migrations fix

--- migrations/2_weicards_migration.js
+++ migrations/2_weicards_migration.js
@@ -3,8 +3,7 @@ const ETHCards = artifacts.require("./WeiCards.sol");
 module.exports = function(deployer, network, accounts) {
   console.log("Deploying to: ", network, accounts)
   // We deploy the contract with the owner being the first address from accounts
-  //const owner = accounts[0]
-  const owner = '0xd90884c83AaB1EdFa43806e059A8735D5D75c574'
+  const owner = accounts[0]
   if (network == "live") {
     const withdrawWallet = ""
     deployer.deploy(ETHCards, owner)
