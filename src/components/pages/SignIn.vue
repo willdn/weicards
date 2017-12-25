b79e0c2bbb11b241e931bd40608c2130effc9129
William Dens <denswilliam@hotmail.com>
2017-12-25 21:36:34 +0100

    Add unlock

--- src/components/pages/SignIn.vue
+++ src/components/pages/SignIn.vue
@@ -10,8 +10,6 @@
       <h2 class="ui basic segment center aligned header">
         You need <img class="ui image tiny" :src="require('@/assets/images/metamask.png')"> <a href="https://metamask.io/">MetaMask</a> to sign in
       </h2>
-      <div class="ui basic segment">
-      </div>
       <h3 class="ui header">
         What is MetaMask ?
       </h3>
@@ -24,6 +22,10 @@
       <p>WeiCards is a Decentralized Application running on the Ethereum blockchain. The easiest way to interact with them is by using a small browser add-on.
         This plugin provides a wallet where you can send some Ether (Ξ) in order to execute operations. This plugin is your only <b>login access</b> to buy, lease and sell cards !
         Thus, there is no registration nor login form, just install and unlock a wallet on MetaMask and you have now access to every WeiCards features.</p>
+      <h3 class="ui header">
+        I already have MetaMask
+      </h3>
+      <p>You need to <b>unlock</b> a wallet in order to access WeiCards.</p>
       <div class="ui basic segment center aligned">
         <div class="ui large header">More questions ? Have a look at our <router-link :to="{ name: 'FAQ' }">FAQ</router-link></div>
       </div>
