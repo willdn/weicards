117d0fb0954224423557425aaa7d669da9f8c51b
Mark Vujevits <vujevits.mark@gmail.com>
2021-08-23 10:07:27 +0200

    fix build

--- src/App.vue
+++ src/App.vue
@@ -15,45 +15,45 @@
 </template>
 
 <script>
-import "@/assets/css/semantic.simplex.css";
-import "@/assets/css/style.css";
-import "../node_modules/izitoast/dist/css/iziToast.min.css";
+import '@/assets/css/semantic.simplex.css'
+import '@/assets/css/style.css'
+import '../node_modules/izitoast/dist/css/iziToast.min.css'
 
-import Navbar from "@/components/Navbar";
-import FooterComponent from "@/components/FooterComponent";
+import Navbar from '@/components/Navbar'
+import FooterComponent from '@/components/FooterComponent'
 // Modals
-import UnknownNetworkModal from "@/components/modals/UnknownNetworkModal";
-import SetLeaseOfferModal from "@/components/modals/SetLeaseOfferModal";
-import BuyModal from "@/components/modals/BuyModal";
-import SellCardModal from "@/components/modals/SellCardModal";
-import CancelSellOfferModal from "@/components/modals/CancelSellOfferModal";
-import CancelLeaseOfferModal from "@/components/modals/CancelLeaseOfferModal";
-import LeaseModal from "@/components/modals/LeaseModal";
+import UnknownNetworkModal from '@/components/modals/UnknownNetworkModal'
+import SetLeaseOfferModal from '@/components/modals/SetLeaseOfferModal'
+import BuyModal from '@/components/modals/BuyModal'
+import SellCardModal from '@/components/modals/SellCardModal'
+import CancelSellOfferModal from '@/components/modals/CancelSellOfferModal'
+import CancelLeaseOfferModal from '@/components/modals/CancelLeaseOfferModal'
+import LeaseModal from '@/components/modals/LeaseModal'
 
-import JSONInterface from "../static/contracts/WeiCards.json";
-import config from "./config";
-import Card from "./api/Card";
-import Web3 from "web3";
+import JSONInterface from '../static/contracts/WeiCards.json'
+import config from './config'
+import Card from './api/Card'
+import Web3 from 'web3'
 
 const networkConfig = {
   live: {
     networkId: 1,
     contractAddress: config.contractAddressMain,
-    fallback: `https://mainnet.infura.io/${config.infuraAPIKey}`,
+    fallback: `https://mainnet.infura.io/${config.infuraAPIKey}`
   },
   ropsten: {
     networkId: 3,
     contractAddress: config.contractAddressRopsten,
-    fallback: `https://ropsten.infura.io/${config.infuraAPIKey}`,
+    fallback: `https://ropsten.infura.io/${config.infuraAPIKey}`
   },
   dev: {
     contractAddress: config.contractAddressDev,
-    fallback: "http://localhost:9545",
-  },
-};
+    fallback: 'http://localhost:9545'
+  }
+}
 
 export default {
-  name: "app",
+  name: 'app',
   components: {
     Navbar,
     FooterComponent,
@@ -63,28 +63,28 @@ export default {
     SellCardModal,
     CancelSellOfferModal,
     CancelLeaseOfferModal,
-    LeaseModal,
+    LeaseModal
   },
-  data() {
+  data () {
     return {
-      network: "live",
-      contract: null,
-    };
+      network: 'live',
+      contract: null
+    }
   },
   watch: {
-    networkUnknown(value) {
+    networkUnknown (value) {
       // if (value) this.$modal.show('unknownNetworkModal')
-    },
+    }
   },
   computed: {
-    isReadOnly() {
+    isReadOnly () {
       return (
         !web3.currentProvider.isMetaMask || !this.$store.getters.currentAddress
-      );
+      )
     },
-    networkUnknown() {
-      return this.$store.getters.networkUnknown;
-    },
+    networkUnknown () {
+      return this.$store.getters.networkUnknown
+    }
   },
   methods: {
     /*
@@ -107,53 +107,53 @@ export default {
     },
     */
     connectWallet: async (_event) => {
-      const [selectedAddress] = await window.ethereum.enable();
-      this.$store.dispatch("setCurrentAddress", selectedAddress);
+      const [selectedAddress] = await window.ethereum.enable()
+      this.$store.dispatch('setCurrentAddress', selectedAddress)
     },
-    setWeb3() {
-      if (typeof web3 !== "undefined") {
-        console.log("Web3 injected browser: OK.");
-        window.web3 = new Web3(window.web3.currentProvider);
+    setWeb3 () {
+      if (typeof web3 !== 'undefined') {
+        console.log('Web3 injected browser: OK.')
+        window.web3 = new Web3(window.web3.currentProvider)
       } else {
-        console.log("Web3 injected browser: Fail. Fallback to INFURA.");
+        console.log('Web3 injected browser: Fail. Fallback to INFURA.')
         window.web3 = new Web3(
           new Web3.providers.HttpProvider(networkConfig[this.network].fallback)
-        );
+        )
       }
-      return window.web3;
+      return window.web3
     },
-    getContract() {
+    getContract () {
       const AppContract = new window.web3.eth.Contract(
         JSONInterface.abi,
         networkConfig[this.network].contractAddress
-      );
-      const contract = Object.freeze(AppContract);
-      this.$store.dispatch("setContract", contract);
-      this.contract = contract;
-      return contract;
+      )
+      const contract = Object.freeze(AppContract)
+      this.$store.dispatch('setContract', contract)
+      this.contract = contract
+      return contract
     },
-    getCurrentAddress() {
+    getCurrentAddress () {
       return window.web3.eth.getAccounts().then((addresses) => {
-        const address = addresses[0];
+        const address = addresses[0]
         if (this.$store.getters.currentAddress !== address) {
-          this.$store.dispatch("setCurrentAddress", address);
+          this.$store.dispatch('setCurrentAddress', address)
         }
-        return address;
-      });
+        return address
+      })
     },
-    getLastLease(id) {
+    getLastLease (id) {
       return this.contract.methods
         .getLastLease(id)
         .call()
         .then((lastLease) => {
-          let _card = Card.getById(id);
-          if (lastLease[0] !== "0") {
-            _card.lastLease = lastLease;
+          let _card = Card.getById(id)
+          if (lastLease[0] !== '0') {
+            _card.lastLease = lastLease
           }
-          return lastLease;
-        });
+          return lastLease
+        })
     },
-    buildCards() {
+    buildCards () {
       for (let i = 1; i <= config.cardsNumber; i++) {
         this.contract.methods
           .getCard(i)
@@ -164,7 +164,7 @@ export default {
               .call()
               .then((details) => {
                 this.$store.dispatch(
-                  "addCard",
+                  'addCard',
                   new Card({
                     id: i,
                     owner: card.owner,
@@ -175,54 +175,54 @@ export default {
                     priceLease: details.priceLease,
                     leaseDuration: details.leaseDuration,
                     availableBuy: details.availableBuy,
-                    availableLease: details.availableLease,
+                    availableLease: details.availableLease
                   })
-                );
-                return this.getLastLease(i);
-              });
+                )
+                return this.getLastLease(i)
+              })
           })
           .catch((err) => {
             // Most certainly because on wrong network
-            this.$store.dispatch("setNetworkUnknown", true);
-            console.log(err.message);
-            return err;
-          });
+            this.$store.dispatch('setNetworkUnknown', true)
+            console.log(err.message)
+            return err
+          })
       }
-      return true;
+      return true
     },
-    getBlockNumber() {
+    getBlockNumber () {
       /* global web3 */
       return web3.eth.getBlockNumber().then((blockNumber) => {
         if (this.$store.getters.blockNumber !== blockNumber) {
-          this.$store.dispatch("setBlockNumber", blockNumber);
+          this.$store.dispatch('setBlockNumber', blockNumber)
         }
-        return blockNumber;
-      });
-    },
+        return blockNumber
+      })
+    }
   },
-  mounted() {
-    this.setWeb3();
-    this.getContract();
+  mounted () {
+    this.setWeb3()
+    this.getContract()
     this.getCurrentAddress()
       .then((address) => {
         // Get block number
-        return this.getBlockNumber();
+        return this.getBlockNumber()
       })
       .then((blockNumber) => {
         // Building cards
-        return this.buildCards();
+        return this.buildCards()
       })
       .then(() => {
         setInterval(() => {
           // Start MetaMask polling
-          this.getCurrentAddress();
+          this.getCurrentAddress()
           // Block number polling
-          this.getBlockNumber();
-        }, 5000);
-        return true;
-      });
-  },
-};
+          this.getBlockNumber()
+        }, 5000)
+        return true
+      })
+  }
+}
 </script>
 
 <style>
