de4eca66b33d71483749c7091bef5a02a7d65869
hexadogg <90278947+hexadogg@users.noreply.github.com>
2021-09-08 20:59:17 +0200

    Fix showing buy price for user owned cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -43,7 +43,7 @@
       </div>
     </div>
     <!-- User Buy -->
-    <div v-if="!card.isWrapped && card.availableBuy && !card.owner.startsWith('0x0000000000000000000000000000000000000000') && card.owner !== currentAddress" class="extra content">
+    <div v-if="!card.isWrapped() && card.availableBuy && !card.owner.startsWith('0x0000000000000000000000000000000000000000') && card.owner !== currentAddress" class="extra content">
       <div class="ui grid equal width center aligned"
            :data-tooltip="`Buy card #${card.id} for ${card.computeInitialPrice().toLocaleString()} Ξ from ${card.owner.substring(0,15)}...`" data-inverted="">
         <div class="column green-hover" @click.prevent="buyCard()">
