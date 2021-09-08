2f710bfa4a48496ce6cf731380e2b5496a9127a4
hexadogg <90278947+hexadogg@users.noreply.github.com>
2021-09-08 15:19:07 +0200

    New contract addresses

--- src/config/index.js
+++ src/config/index.js
@@ -6,12 +6,12 @@ const config = {
   contractAddressDev: '0x345ca3e014aaf5dca488057592ee47305d9b3e10', // developement
   contractAddressMain: '0x7F57292bF494A8c9342d37395D1378A65D59C499', // main
   contractAddressRopsten: '0x89176Ca0bA94BbED8A2399aAD2D521b6A5Cf944A', // ropsten
-  contractAddressRinkeby: '0x38E73e2Ac25904C02c59386Eff8BAd4F5054EBE7', // rinkeby
+  contractAddressRinkeby: '0x0EB0651ffb92B48D6756911Cec9367c024a99994', // rinkeby
 
   wrapContractAddressDev: '', // developement
   wrapContractAddressMain: '', // main
   wrapContractAddressRopsten: '', // ropsten
-  wrapContractAddressRinkeby: '0xD7aC3BBA9D63Bb66d73628862d1E270E9431bD6B' // rinkeby
+  wrapContractAddressRinkeby: '0x6a3EB1D26be168469E51F0e7eA778Abc216Bd504' // rinkeby
 }
 
 export default config
